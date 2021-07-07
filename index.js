4769ea1b99195bf6e28de0a2d70a9afcadc318df
Duy Nhat <duynhat719@gmail.com>
2021-07-07 18:16:22 +0700

    Done

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ server.use(middlewares);
 
 // Add custom routes before JSON Server router
 server.get('/echo', (req, res) => {
-  res.jsonp(req.query);
+    res.jsonp(req.query);
 });
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now();
-    req.body.updatedAt = Date.now();
-  } else if (req.method === 'PATCH') {
-    req.body.updatedAt = Date.now();
-  }
-  // Continue to JSON Server router
-  next();
+    if (req.method === 'POST') {
+        req.body.createdAt = Date.now();
+        req.body.updatedAt = Date.now();
+    } else if (req.method === 'PATCH') {
+        req.body.updatedAt = Date.now();
+    }
+    // Continue to JSON Server router
+    next();
 });
 
 const PORT = process.env.PORT || 3000;
@@ -30,5 +30,5 @@ const PORT = process.env.PORT || 3000;
 // Use default router
 server.use('/api', router);
 server.listen(PORT, () => {
-  console.log('JSON Server is running');
+    console.log('JSON Server is running');
 });
